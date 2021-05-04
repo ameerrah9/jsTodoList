88bf7c9ab912d07578be75527b69af0bf51dd629
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2021-05-03 22:24:47 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class List extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            todos: [
+                {id: 1, content: "Task #1"},
+                {id: 2, content: "Task #2"}
+            ]
+        };
+
+    }
+
+    componentDidMount() {
+        // this method is called AFTER the component is rendered onto the DOM
+    }
+
+    deleteTodo = id => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        });
+
+        this.setState({
+            todos
+        })
+    }
+    render() {
+        return(
+            <div className="List">
+                <h1 className="center blue-text">Todo's</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+            </div>
+        );
+    }
+}
+
+// ===================================================
+
+const Todos = ({ todos, deleteTodo }) => {
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">You have no todo's left</p>
+    )
+
+    return (
+        <div className="todos collection">
+            {todoList}
+        </div>
+    )
+}
+
+// ===================================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <List />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
